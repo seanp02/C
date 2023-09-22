aca7dc712ffef083f61e727a8a1c49004003386e
seanp02 <seanp02@naver.com>
2023-09-22 17:57:16 +0900

    hw2.c

--- hw2.c
+++ hw2.c
@@ -6,7 +6,7 @@ int main(){
     double mile;
     printf("Please enter kilometers: ");
     scanf("%lf", &km);
-    mile = 0.6214 * km;
+    mile = km / 1.609;
     printf("%.1f km is equal to %.1f miles\n", km, mile);
 
     return 0;
