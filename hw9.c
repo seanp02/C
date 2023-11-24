30bccbb3197ee12412469f67749121108872b9e0
seanp02 <seanp02@naver.com>
2023-11-24 22:29:15 +0900

    hw9.c

---
+++ hw9.c
@@ -0,0 +1,23 @@
+#define _CTR_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+int convCase(int ch) {
+    const int diff = 'a' - 'A';
+    if (ch >= 'A' && ch <= 'Z')
+        return ch + diff;
+    else if (ch >= 'a' && ch <= 'z')
+        return ch - diff;
+    else
+        return ch;
+}
+int main() {
+    char str[100];
+    int i;
+    printf("Input> ");
+    fgets(str, sizeof(str), stdin);
+    for(i=0; i<strlen(str); i++) {
+        str[i]=convCase(str[i]);
+    }
+    printf("%s", str);
+    return 0;
+}
